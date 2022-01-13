f0dd7cd26353a6dff55ad59dd00cd9987968f3cf
Southier <patrick.southier@hotmail.com>
2022-01-13 04:05:02 -0300

    feat: implementing get_unique_industries at insights.py

--- src/insights.py
+++ src/insights.py
@@ -28,21 +28,12 @@ def filter_by_job_type(jobs, job_type):
 
 
 def get_unique_industries(path):
-    """Checks all different industries and returns a list of them
-
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    list
-        List of unique industries
-    """
-    return []
+    unique_industries_list = set()
+    jobs_list = read(path)
+    for job_row in jobs_list:
+        if job_row["industry"] != '':
+            unique_industries_list.add(job_row["industry"])
+    return unique_industries_list
 
 
 def filter_by_industry(jobs, industry):
