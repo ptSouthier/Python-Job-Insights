888a8825cdaae6d7fb3ebbf5dc321f8e8c47be48
Southier <patrick.southier@hotmail.com>
2022-01-13 04:49:51 -0300

    feat: implementing get_max_salary at insights.py

--- src/insights.py
+++ src/insights.py
@@ -55,21 +55,17 @@ def filter_by_industry(jobs, industry):
 
 
 def get_max_salary(path):
-    """Get the maximum salary of all jobs
+    max_salary = 0
+    jobs_list = read(path)
+    for job_row in jobs_list:
+        try:
+            if int(job_row["max_salary"]) > max_salary:
+                max_salary = int(job_row["max_salary"])
+        except ValueError:
+            pass
 
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    int
-        The maximum salary paid out of all job opportunities
-    """
-    pass
+    return max_salary
+    # pass
 
 
 def get_min_salary(path):
