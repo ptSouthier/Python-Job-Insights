5dcf6077a977ffa6396685dc97abb3217ef414d6
Southier <patrick.southier@hotmail.com>
2022-01-13 21:16:30 -0300

    feat: implementing test_sort_by_criteria at test_sorting

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,5 +1,40 @@
-# from src.sorting import sort_by
+# import pytest
+import pytest
+from src.sorting import sort_by
+
+JOBS_MOCK = [
+        {"min_salary": 500, "max_salary": 5000, "date_posted": "2022-01-13"},
+        {"min_salary": 300, "max_salary": 3000, "date_posted": "2021-11-02"},
+        {"min_salary": 600, "max_salary": 3200, "date_posted": "2021-08-22"},
+    ]
+
+INVALID_CRITERIAS = ["", "industry", None, 0]
 
 
 def test_sort_by_criteria():
-    pass
+    sort_by(JOBS_MOCK, "min_salary")
+    assert JOBS_MOCK == [
+        {"min_salary": 300, "max_salary": 3000, "date_posted": "2021-11-02"},
+        {"min_salary": 500, "max_salary": 5000, "date_posted": "2022-01-13"},
+        {"min_salary": 600, "max_salary": 3200, "date_posted": "2021-08-22"},
+    ]
+
+    sort_by(JOBS_MOCK, "max_salary")
+    assert JOBS_MOCK == [
+        {"min_salary": 500, "max_salary": 5000, "date_posted": "2022-01-13"},
+        {"min_salary": 600, "max_salary": 3200, "date_posted": "2021-08-22"},
+        {"min_salary": 300, "max_salary": 3000, "date_posted": "2021-11-02"},
+    ]
+
+    sort_by(JOBS_MOCK, "date_posted")
+    assert JOBS_MOCK == [
+        {"min_salary": 500, "max_salary": 5000, "date_posted": "2022-01-13"},
+        {"min_salary": 300, "max_salary": 3000, "date_posted": "2021-11-02"},
+        {"min_salary": 600, "max_salary": 3200, "date_posted": "2021-08-22"},
+    ]
+
+    for criteria in INVALID_CRITERIAS:
+        with pytest.raises(
+            ValueError, match=f"invalid sorting criteria: {criteria}"
+        ):
+            sort_by(JOBS_MOCK, criteria)
